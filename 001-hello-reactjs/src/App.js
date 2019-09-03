c09e2bb875939d8b42f38e55599b8d36bb0263af
mimaraslan <mimaraslan@gmail.com>
2019-09-03 21:50:41 +0300

    ReactJS
    ReactJS

--- 001-hello-reactjs/src/App.js
+++ 001-hello-reactjs/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
          <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello ReactJS
         </p>
       </header>
     </div>
