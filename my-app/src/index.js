04b1c3d8dfe1b7b735685567f9b04592f9d116f4
mimaraslan <mimaraslan@gmail.com>
2020-04-29 15:27:57 +0300

    Hello ReactJS
    Hello ReactJS

--- my-app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import Example from './Example';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Example />, document.getElementById('root'));
-registerServiceWorker();
