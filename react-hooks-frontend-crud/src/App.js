9706364509fb8d13074577439c5d61a107b38ab8
mimaraslan <mimaraslan@gmail.com>
2020-09-08 19:03:30 +0300

    React Hooks Frontend CRUD Bootstrap
    React Hooks Frontend CRUD Bootstrap

--- react-hooks-frontend-crud/src/App.js
+++ react-hooks-frontend-crud/src/App.js
@@ -7,16 +7,7 @@ function App() {
         <p>
           Hello React
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          </a>
 
-Bootstrap
 <button type="button" class="btn btn-dark">Dark</button>
 <button type="button" class="btn btn-danger">Danger</button>
 <button type="button" class="btn btn-warning">Warning</button>
