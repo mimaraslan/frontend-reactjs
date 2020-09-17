2e6cd24675d8756fe9b76f1e6989dce5a648b56b
mimaraslan <mimaraslan@gmail.com>
2020-09-17 21:57:05 +0300

    React Hooks Frontend CRUD
    React Hooks Frontend CRUD

--- react-hooks-frontend-crud/src/App.js
+++ react-hooks-frontend-crud/src/App.js
@@ -1,18 +1,10 @@
 import './App.css';
+import ListEmployeeComponent from './components/ListEmployeeComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello React
-        </p>
-
-<button type="button" class="btn btn-dark">O</button>
-<button type="button" class="btn btn-danger">Gemi</button>
-<button type="button" class="btn btn-warning">Gelecek</button>
-
-      </header>
+    <div>
+      <ListEmployeeComponent />
     </div>
   );
 }
--- react-hooks-frontend-crud/src/components/ListEmployeeComponent.js
+++ react-hooks-frontend-crud/src/components/ListEmployeeComponent.js
@@ -1,35 +1,35 @@
 import React, {useState} from 'react'
 const ListEmployeeComponent = () => {
 
-  const [employees, setEmployess] = useState([]),
+  const [employees, setEmployees] = useState([])
 
-  return
-  <div className="container">
-    <h2 className="text-center">List Employess</h2>
-    <table className="table table-bordered table-striped">
-      <thead>
-        <th> Id</th>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Email</th>
-      </thead>
-      <tbody>
-        {
-          employees.map(
-            employee =>
-              <tr key={employee.id}>
-                <td>{employee.id}</td>
-                <td>{employee.firstName}</td>
-                <td>{employee.lastName}</td>
-                <td>{ employee.emailId}</td>
-              </tr>
-          )
-        }
-      </tbody>
-    </table>
-   </div>
-    )
-
+  return (
+    <div className = "container">
+        <h2 className = "text-center"> List Employees </h2>
+        <table className="table table-bordered table-striped">
+            <thead>
+                <th> Id </th>
+                <th> First Name </th>
+                <th> Last Name </th>
+                <th> Email Id </th>
+                <th> Actions </th>
+            </thead>
+            <tbody>
+                {
+                    employees.map(
+                        employee =>
+                        <tr key = {employee.id}>
+                            <td> {employee.id} </td>
+                            <td> {employee.firstName} </td>
+                            <td>{employee.lastName}</td>
+                            <td>{employee.emailId}</td>
+                        </tr>
+                    )
+                }
+            </tbody>
+        </table>
+    </div>
+)
 }
 
 export default ListEmployeeComponent
