e1cf25de3aefb1f7a86700b5dffe4e31d508114d
mimaraslan <mimaraslan@gmail.com>
2020-09-18 01:17:04 +0300

    React Hooks Frontend CRUD
    React Hooks Frontend CRUD

---
+++ react-hooks-frontend-crud/src/services/EmployeeService.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const EMPLOYEE_BASE_REST_API_URL = "http://localhost:8082/api/v1/employees"
+
+class EmployeeServies{
+
+    getAllEmployees(){
+        return axios.get(EMPLOYEE_BASE_REST_API_URL)
+    }
+
+}
+
+export default new EmployeeServies();
