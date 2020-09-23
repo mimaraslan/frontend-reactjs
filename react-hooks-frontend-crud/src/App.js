076dbed6b6ca6148e135a8a77c1f2d8e65e00778
mimaraslan <mimaraslan@gmail.com>
2020-09-23 03:08:29 +0300

    React Hooks Frontend CRUD
    React Hooks Frontend CRUD

--- react-hooks-frontend-crud/src/App.js
+++ react-hooks-frontend-crud/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import FooterComponent from './components/FooterComponent';
+import HeaderComponent from './components/HeaderComponent';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 
 function App() {
   return (
     <div>
-      <ListEmployeeComponent />
+      <Router>
+        <HeaderComponent/>
+          <div className="container">
+            <Routes>
+              <Route exact path="/" element= {<ListEmployeeComponent/>}/>
+              <Route exact path="/employees" element= {<ListEmployeeComponent/>}/>
+            </Routes>
+          </div>
+          <FooterComponent/>
+      </Router>
     </div>
   );
 }
