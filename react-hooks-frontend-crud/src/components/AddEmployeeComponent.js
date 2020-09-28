4ce68278e12aa074bbb39c171d27772646467ce5
mimaraslan <mimaraslan@gmail.com>
2020-09-28 03:43:28 +0300

    React Hooks Frontend CRUD
    React Hooks Frontend CRUD

---
+++ react-hooks-frontend-crud/src/components/AddEmployeeComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddEmployeeComponent = () => {
+  return (
+    <div>
+        <h1>Add Employee Component Called</h1>
+    </div>
+  )
+}
+
+export default AddEmployeeComponent
