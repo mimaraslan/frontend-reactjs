59f72b05c10843465043c9050751fdd99f5d53d8
mimaraslan <mimaraslan@gmail.com>
2020-10-08 03:31:53 +0300

    React Hooks Frontend CRUD
    React Hooks Frontend CRUD

--- react-hooks-frontend-crud/src/components/ListEmployeeComponent.js
+++ react-hooks-frontend-crud/src/components/ListEmployeeComponent.js
@@ -20,39 +20,36 @@ const ListEmployeeComponent = () => {
     }
 
     const deleteEmployee = (employeeId) => {
-       EmployeeService.deleteEmployee(employeeId).then((response) =>{
+        EmployeeService.deleteEmployee(employeeId).then((response) =>{
         getAllEmployees();
-
-       }).catch(error =>{
-           console.log(error);
-       })
-        
+        }).catch(error =>{
+            console.log(error);
+        })
     }
 
     return (
         <div className = "container">
             <h2 className = "text-center"> List Employees </h2>
-            <Link to="/add-employee" className="btn btn-primary mb-2">Add New</Link>
+            <Link to = "/add-employee" className = "btn btn-primary mb-2" > Add Employee </Link>
             <table className="table table-bordered table-striped">
                 <thead>
-                    <tr>
-                        <th>Id </th>
-                        <th>First Name </th>
-                        <th>Last Name </th>
-                        <th>Email Id </th>
-                        <th> Actions </th>
-                    </tr>
+                    <th> Employee Id </th>
+                    <th> Employee First Name </th>
+                    <th> Employee Last Name </th>
+                    <th> Employee Email Id </th>
+                    <th> Actions </th>
                 </thead>
                 <tbody>
                     {
                         employees.map(
                             employee =>
-                            <tr key = {employee.id}>
+                            <tr key = {employee.id}> 
                                 <td> {employee.id} </td>
                                 <td> {employee.firstName} </td>
                                 <td>{employee.lastName}</td>
                                 <td>{employee.emailId}</td>
                                 <td>
+                                    <Link className="btn btn-info" to={`/edit-employee/${employee.id}`} >Update</Link>
                                     <button className = "btn btn-danger" onClick = {() => deleteEmployee(employee.id)}
                                     style = {{marginLeft:"10px"}}> Delete</button>
                                 </td>
@@ -60,15 +57,6 @@ const ListEmployeeComponent = () => {
                         )
                     }
                 </tbody>
-                <tfoot>
-                    <tr>
-                        <td>Sum</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                </tfoot>
             </table>
         </div>
     )
