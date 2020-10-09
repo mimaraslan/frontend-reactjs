2a819ed5bce8670118f43bed4cef56533f8efa20
mimaraslan <mimaraslan@gmail.com>
2020-10-09 03:32:04 +0300

    React Hooks Frontend CRUD
    React Hooks Frontend CRUD

--- react-hooks-frontend-crud/src/components/AddEmployeeComponent.js
+++ react-hooks-frontend-crud/src/components/AddEmployeeComponent.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import EmployeeService from '../services/EmployeeService'
 import {Link, useNavigate, useParams } from 'react-router-dom';
 
@@ -8,6 +8,8 @@ const [firstName, setFirstName] = useState('')
 const [lastName, setLastName] = useState('')
 const [emailId, setEmailId] = useState('')
 const navigate = useNavigate()
+const {id} = useParams();
+/*
 const saveEmployee = (e) => {
   e.preventDefault();
   const employee = {firstName, lastName, emailId}
@@ -18,6 +20,46 @@ const saveEmployee = (e) => {
     console.log(error)
   })
 }
+*/
+const saveOrUpdateEmployee = (e) => {
+  e.preventDefault();
+
+  const employee = {firstName, lastName, emailId}
+
+  if(id){
+      EmployeeService.updateEmployee(id, employee).then((response) => {
+        navigate('/employees')
+      }).catch(error => {
+          console.log(error)
+      })
+  }else{
+      EmployeeService.createEmployee(employee).then((response) =>{
+          console.log(response.data)
+          navigate('/employees');
+      }).catch(error => {
+          console.log(error)
+      })
+  }
+}
+
+useEffect(() => {
+
+  EmployeeService.getEmployeeById(id).then((response) =>{
+      setFirstName(response.data.firstName)
+      setLastName(response.data.lastName)
+      setEmailId(response.data.emailId)
+  }).catch(error => {
+      console.log(error)
+  })
+}, [])
+
+const title = () => {
+  if(id){
+      return <h2 className = "text-center">Update Employee</h2>
+  }else{
+      return <h2 className = "text-center">Add Employee</h2>
+  }
+}
 
   return (
     <div>
@@ -25,7 +67,7 @@ const saveEmployee = (e) => {
       <div className="container">
           <div className="row">
             <div className="card col-md-6 ofset-md-3 ofset-md-3 ">
-              <h2 className="text-center">Add Employee</h2>
+              { title() }
               <div className="card-body">
                 <form>
                   <div className="form-group mb-2">
@@ -61,8 +103,8 @@ const saveEmployee = (e) => {
                       >
                     </input>
                   </div>
-                  <button className="btn btn-success" onClick={(e) => saveEmployee(e)}> Submit</button>
-                  <Link to ="/employees" className="btn btn-danger">Cancel</Link>               
+                  <button className = "btn btn-success" onClick = {(e) => saveOrUpdateEmployee(e)} >Submit </button>
+                  <Link to ="/employees" className="btn btn-danger">Cancel</Link>
                 </form>
               </div>
             </div>
